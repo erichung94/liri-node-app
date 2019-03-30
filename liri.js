3c73512de95ec8404e6ab9c1837b50b138adba86
unknown <erichung.94@gmail.com>
2019-03-29 22:13:02 -0500

    added spotify and do-what-it-says functions & organized functions

--- liri.js
+++ liri.js
@@ -2,74 +2,117 @@ var fs = require('fs');
 var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var command = process.argv[2]
+var input = process.argv.slice(3).join(" ");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var moment = require("moment");
 var axios = require("axios");
 
-switch(command){
+userInputs(command, input);
+function userInputs(command, input){
+    switch(command){
+        case "concert-this":
+            concertInfo(input);
+        break;
+        
+        case "spotify-this-song":
+            spotifyInfo(input);
+        break;
+        
+        case "movie-this":
+            movieInfo(input);
+        break;
 
-case "concert-this":
-var artist = process.argv.slice(3).join(" "); 
-var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-axios({
-    method: 'get',
-    url: queryURL,
-    responseType: 'JSON'
-})
-.then(function(response, err){
-    console.log("\nVenue: " + response.data[0].venue.name);
-    console.log("Located at " + response.data[0].venue.city + ", " + response.data[0].venue.region);
-})
+        case "do-what-it-says":
+            doWhatInfo();
+        break;
 
-break;
-
-//case "spotify-this-song":
-//var songName = process.argv.slice(3).join(" ");
-
-
-
-//break;
-
-case "movie-this":
-var movieName = process.argv.slice(3).join(" ");
-var queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movieName;
-
-axios({
-    method: 'get',
-    url: queryURL,
-    responseType: 'JSON'
-})
-.then(function(response,err){
-    var a = response.data;
-    function printMovie(){
-        console.log("\nMovie Title: " + a.Title + "\nYear Released: " + a.Year + 
-        "\nIMDB Rating: " + a.Ratings[0].Value + "\nRotten Tomatos Rating: " + a.Ratings[1].Value +
-        "\nProduced in: " + a.Country + "\nLanguage: " + a.Language +
-        "\nPlot: " + a.Plot + "\nActors: " + a.Actors);
+        default: 
+            console.log("Invalid option. Please type in valid option.");
+        break;
     }
-    if (a.Response === "False"){
-        var queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=Mr.Nobody";
+}
+
+function concertInfo(input){ 
+        var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+        var formattedDate = "";
         axios({
             method: 'get',
             url: queryURL,
             responseType: 'JSON'
         })
-        .then(function(response,err){
-            printMovie();
+        .then(function(response, err){
+            for (i=0; i<3; i++){
+            formattedDate = moment(response.data[i].datetime).format('LL');
+            console.log("--------------------------------------------------------------------------");
+            console.log("\nVenue: " + response.data[i].venue.name);
+            console.log("Located at " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+            console.log("Date: " + formattedDate + "\n");
+            console.log("--------------------------------------------------------------------------");
+            }
+        })
+}
+function spotifyInfo(input){
+    if (input == ""){
+        input = "The Sign";
+    }
+    spotify.search(
+        {
+            type: "track",
+            query: input
+        },
+        function (err, data){
+        var songs = data.tracks.items;
+            for (i=0;i<songs.length;i++){
+            console.log("--------------------------------------------------------------------------");
+            console.log(i);
+            console.log("Artist(s): " + songs[i].artists[0].name);
+            console.log("Name of the song: " + songs[i].name);
+            console.log("Preview Link: " + songs[i].preview_url);
+            console.log("Album: " + songs[i].album.name + "\n");
+            console.log("--------------------------------------------------------------------------");  
+            }
+        }
+    ) 
+}
+
+function movieInfo(input){
+    if (input === ""){
+        input = "Mr. Nobody";
+        console.log("If you haven't watched Mr. Nobody, then you should!");
+        console.log("Here's some info on it, it's on Netflix!");
+    }
+
+    var queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + input;
+     axios({
+        method: 'get',
+        url: queryURL,
+        responseType: 'JSON'
+    })
+    .then(function(response,err){
+        var movie = response.data;
+        if (movie.Ratings === undefined){
+            console.log("\nNot a valid movie name!");
+            return;
+        }
+        else{
+            console.log("--------------------------------------------------------------------------");
+            console.log("\nMovie Title: " + movie.Title + "\nYear Released: " + movie.Year + 
+            "\nIMDB Rating: " + movie.Ratings[0].Value + "\nRotten Tomatos Rating: " + movie.Ratings[1].Value +
+            "\nProduced in: " + movie.Country + "\nLanguage: " + movie.Language +
+            "\nPlot: " + movie.Plot + "\nActors: " + movie.Actors +"\n");
+            console.log("--------------------------------------------------------------------------");
+        }
+
+    })    
+}
+ 
+function doWhatInfo(){
+    fs.readFile('random.txt', 'utf8', function(err, data){
+        if (err){ 
+            return console.log(err);
+        }
+        var dataArr = data.split(',');
+        userInputs(dataArr[0], dataArr[1]);
     });
-    }
-    else {
-    printMovie();
-    }
-})
-
-break;
-
-case "do-what-it-says":
-//Runs the command on random.txt.
-
-break;
-
-default:
 }
